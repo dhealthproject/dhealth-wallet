eca48da159c50ec7f176627af0f7c138b5ea572f
Rebecca Natterer <natterer.re@gmail.com>
2021-07-01 17:09:39 +0200

    WIP added category subtext, added faq search, added translations

--- src/views/pages/faq/Dashboard/Dashboard.vue
+++ src/views/pages/faq/Dashboard/Dashboard.vue
@@ -6,69 +6,86 @@
             <div class="left-container">
                 <div class="header-container">
                     <NavigationTabs direction="horizontal" :parent-route-name="parentRouteName" />
+                    <div class="search-container">
+                        <form action="">
+                            <input type="text" placeholder="Search.." name="search">
+                            <button type="submit"><img src="@/views/resources/img/icons/Search.svg" /></button>
+                        </form>
+                    </div>
                 </div>
                 <div class="bottom-container">
                     <div class="faq-main-container">
                         <div class="faq-container">
-                            
-                            <details open class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_first') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_first')"></div>
-                            </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_second') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_second')"></div>
-                            </details>
+                            <div class="faq-items-first">
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_third') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_third')"></div>
-                            </details>
+                                <details open class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_first') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_first')"></div>
+                                </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_fourth') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_fourth')"></div>
-                            </details>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_second') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_second')"></div>
+                                </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_fifth') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_fifth')"></div>
-                            </details>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_third') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_third')"></div>
+                                </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_sixth') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_sixth')"></div>
-                            </details>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_fourth') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_fourth')"></div>
+                                </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_seventh') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_seventh')"></div>
-                            </details>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_fifth') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_fifth')"></div>
+                                </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_eighth') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_eighth')"></div>
-                            </details>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_sixth') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_sixth')"></div>
+                                </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_ninth') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_ninth')"></div>
-                            </details>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_seventh') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_seventh')"></div>
+                                </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_tenth') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_tenth')"></div>
-                            </details>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_eighth') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_eighth')"></div>
+                                </details>
 
-                            <details class="faq-item">
-                                <summary class="faq-title">{{ $t('faq_question_eleventh') }}</summary>
-                                <div class="faq-content" v-html="$t('faq_answer_eleventh')"></div>
-                            </details>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_ninth') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_ninth')"></div>
+                                </details>
 
-                            <div class="faq-item faq-contact-item">
-                                <div class="faq-title">{{ $t('faq_contact_title') }}</div>
-                                <div class="faq-content" v-html="$t('faq_contact_content')"></div>
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_tenth') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_tenth')"></div>
+                                </details>
+
+                                <details class="faq-item">
+                                    <summary class="faq-title">{{ $t('faq_question_eleventh') }}</summary>
+                                    <div class="faq-content" v-html="$t('faq_answer_eleventh')"></div>
+                                </details>
+
+                            </div>
+
+                            <div class="faq-items-second">
+                                <details open class="faq-item">
+                                    <summary class="faq-title">ITEM 2 HEADING</summary>
+                                    <div class="faq-content">ITEM 2 TEXT</div>
+                                </details>
+                            </div>
+
+                            <div class="faq-contact-item">
+                                <div class="faq-contact-title">{{ $t('faq_contact_title') }}</div>
+                                <div class="faq-contact-content" v-html="$t('faq_contact_content')"></div>
                             </div>
 
                         </div>
@@ -78,27 +95,44 @@
 
             <div class="right-container">
                 <div class="header-container">
-                    <div class="faq-category-title">{{ $t('faq_category-title') }}</div>
+                    <div class="faq-category-title">{{ $t('faq_category_title') }}</div>
                 </div>
                 <div class="bottom-container">
                     <div class="faq-category-main-container">
                         <div class="faq-category-container">
-                            <div class="faq-category-item grow">
-                                <img class="faq-category-img" src="@/views/resources/img/icons/metadata.png" />
-                                <div class="faq-category-text">{{ $t('faq_category-first') }}</div>
+
+                            <div class="faq-category-item">
+                                <div class="faq-category-head grow">
+                                    <img class="faq-category-img" src="@/views/resources/img/icons/metadata.png" />
+                                    <div class="faq-category-text">{{ $t('faq_category_first') }}</div>
+                                </div>
+                                <div class="faq-category-subtext">{{ $t('faq_category_sub_first') }}</div>
                             </div>
-                            <div class="faq-category-item grow">
-                                <img class="faq-category-img" src="@/views/resources/img/icons/mosaic.png" />
-                                <div class="faq-category-text">{{ $t('faq_category-second') }}</div>
+
+                            <div class="faq-category-item">
+                                <div class="faq-category-head grow">
+                                    <img class="faq-category-img" src="@/views/resources/img/icons/mosaic.png" />
+                                    <div class="faq-category-text">{{ $t('faq_category_second') }}</div>
+                                </div>
+                                <div class="faq-category-subtext">{{ $t('faq_category_sub_second') }}</div>
                             </div>
-                            <div class="faq-category-item grow">
-                                <img class="faq-category-img" src="@/views/resources/img/icons/blocks.png" />
-                                <div class="faq-category-text">{{ $t('faq_category-third') }}</div>
+
+                            <div class="faq-category-item">
+                                <div class="faq-category-head grow">
+                                    <img class="faq-category-img" src="@/views/resources/img/icons/blocks.png" />
+                                    <div class="faq-category-text">{{ $t('faq_category_third') }}</div>
+                                </div>
+                                <div class="faq-category-subtext">{{ $t('faq_category_sub_third') }}</div>
                             </div>
-                            <div class="faq-category-item grow">
-                                <img class="faq-category-img" src="@/views/resources/img/icons/wallet.png" />
-                                <div class="faq-category-text">{{ $t('faq_category-fourth') }}</div>
+
+                            <div class="faq-category-item">
+                                <div class="faq-category-head grow">
+                                    <img class="faq-category-img" src="@/views/resources/img/icons/wallet.png" />
+                                    <div class="faq-category-text">{{ $t('faq_category_fourth') }}</div>
+                                </div>
+                                <div class="faq-category-subtext">{{ $t('faq_category_sub_fourth') }}</div>
                             </div>
+
                         </div>
                     </div>
 
